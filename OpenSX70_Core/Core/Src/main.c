f9fd8ff7319c24c702bdf9016ab6745786efb551
Zane Pollard <pollardzane@gmail.com>
2025-12-07 16:43:05 -0600

    solenoid initialization

--- OpenSX70_Core/Core/Src/main.c
+++ OpenSX70_Core/Core/Src/main.c
@@ -23,6 +23,7 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "opensx70.h"
+#include "camerafunctions.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -102,6 +103,7 @@ int main(void)
   MX_USART1_UART_Init();
   MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
+  solenoid_init();
 
   /* USER CODE END 2 */
 
