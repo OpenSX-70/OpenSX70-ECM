711c06e2ff6d7a3f195b033134f306306c856f7a
Zane Pollard <pollardzane@gmail.com>
2025-12-07 18:05:24 -0600

    fleshing out functions

--- OpenSX70_Core/Core/Src/camerafunctions.c
+++ OpenSX70_Core/Core/Src/camerafunctions.c
@@ -1,5 +1,6 @@
 #include "camerafunctions.h"
 #include "main.h"
+#include "settings.h"
 
 void solenoid_init(void){
     HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
@@ -8,6 +9,8 @@ void solenoid_init(void){
 
 void shutter_close(){
     __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 255);
+    HAL_Delay(POWER_DOWN_DELAY);
+    __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 77);
 }
 
 void shutter_open(){
@@ -34,4 +37,27 @@ void mirror_down(){
     }
 
     HAL_GPIO_WritePin(MOTOR_GPIO_Port, MOTOR_Pin, GPIO_PIN_RESET);
+}
+
+void mirror_up(){
+    if(HAL_GPIO_ReadPin(S3_GPIO_Port, S3_Pin) != GPIO_PIN_SET){
+        HAL_GPIO_WritePin(MOTOR_GPIO_Port, MOTOR_Pin, GPIO_PIN_SET);
+    }
+
+    while(HAL_GPIO_ReadPin(S5_GPIO_Port, S5_Pin) != GPIO_PIN_SET){
+        //Wait for S5 to go high
+    }
+
+    HAL_GPIO_WritePin(MOTOR_GPIO_Port, MOTOR_Pin, GPIO_PIN_RESET);
+}
+
+void darkslide_eject(){
+    shutter_close();
+    mirror_up();
+    mirror_down();
+    shutter_open();
+}
+
+void auto_exposure(meter_iso iso_setting){
+
 }
