6ae67aba5d0a13dab0861cf83521ce6f4c843f0c
Zane Pollard <pollardzane@gmail.com>
2025-12-10 17:32:53 -0600

    Clocksource update, generated TIM3 with cubemx properly

--- OpenSX70_Core/Core/Src/main.c
+++ OpenSX70_Core/Core/Src/main.c
@@ -345,11 +345,11 @@ static void MX_TIM3_Init(void)
   /* USER CODE BEGIN TIM3_Init 0 */
   /* USER CODE END TIM3_Init 0 */
 
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
   TIM_MasterConfigTypeDef sMasterConfig = {0};
 
   /* USER CODE BEGIN TIM3_Init 1 */
   /* USER CODE END TIM3_Init 1 */
-
   htim3.Instance = TIM3;
   htim3.Init.Prescaler = 3663;
   htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
@@ -360,7 +360,11 @@ static void MX_TIM3_Init(void)
   {
     Error_Handler();
   }
-
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim3, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
   sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
   sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
   if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
