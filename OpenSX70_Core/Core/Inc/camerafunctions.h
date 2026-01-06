da6d35336ebb83ea8ffcf680bece70048de2e7d9
Zane Pollard <pollardzane@gmail.com>
2026-01-05 18:27:56 -0600

    removing multiple exposure finish comment from camerafunctions header

--- OpenSX70_Core/Core/Inc/camerafunctions.h
+++ OpenSX70_Core/Core/Inc/camerafunctions.h
@@ -34,7 +34,6 @@ void manual_exposure(uint8_t selector_value);
 void bulb_mode(void);
 void time_mode(void);
 void exposure_finish(void);
-//void multiple_exposure_finish();
 void flash(void);
 void s2_ffa_mode(void);
 void s2_usart_mode(void);
