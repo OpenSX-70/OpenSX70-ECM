29d939a118251772fd5803b81dcad8a0acce345c
Zane Pollard <pollardzane@gmail.com>
2026-04-21 14:13:09 -0500

    changing things back

--- OpenSX70_Core/Core/Src/settings.c
+++ OpenSX70_Core/Core/Src/settings.c
@@ -5,5 +5,5 @@
 const uint8_t Y_DELAY = 40;
 const uint8_t POWER_DOWN_DELAY = 28;
 
-const int32_t ShutterSpeed[] = {     26,   32,   38,    44,  48,   52,  56,   66,  125,  250, 500, 1000, POST, POSB, AUTO, AUTO_F};
+const int32_t ShutterSpeed[] = {     23,   27,   32,    34,  39,   44,  54,   66,  125,  250, 500, 1000, POST, POSB, AUTO, AUTO_F};
 //                                  2000  1000   500    250  125    60   30    15     8     4    2     1
